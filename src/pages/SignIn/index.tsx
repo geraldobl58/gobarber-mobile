f1289b267e88507b2d576049a8789a4b1c854d65
Geraldo Luiz <geraldobl58@gmail.com>
2021-03-23 18:34:09 -0300

    feat: Registration and navigation screen

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
+import {useNavigation} from '@react-navigation/native';
+
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
@@ -24,6 +26,8 @@ import {
 import logoImg from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
     <>
       <KeyboardAvoidingView
@@ -52,7 +56,7 @@ const SignIn: React.FC = () => {
         </ScrollView>
       </KeyboardAvoidingView>
 
-      <CreateAccountButton onPress={() => {}}>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
         <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
       </CreateAccountButton>
